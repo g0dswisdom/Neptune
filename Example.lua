1175de4b6b563b40e7762f08de9ff2b0a6959d7d
g0dswisdom <128635829+g0dswisdom@users.noreply.github.com>
2023-05-25 17:51:25 +0300

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,2 @@
+local neptune = require(script.Parent.Neptune) -- ModuleScript Position (You can also use Neptune for exploits you just have to change some stuff)
+neptune.newNotification({title="Title", position=UDim2.new(0.388779521, 0, 0.282419771, 0), message="Message"})
